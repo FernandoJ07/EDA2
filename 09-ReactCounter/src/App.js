80551eecce3b1b8719b97d004ca682e3cb37e1cd
FernandoJ07 <fernandogerdez@gmail.com>
2024-10-10 11:59:50 -0500

    Uploading Files

---
+++ 09-ReactCounter/src/App.js
@@ -0,0 +1,15 @@
+import FirstApp from "./FirstApp";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FirstApp 
+        value={0}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
